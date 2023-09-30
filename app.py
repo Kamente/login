ded016071346935e72703644e1a3ebf6ffc44e43
Kamente <justinm48000@gmail.com>
2023-09-30 22:06:37 +0300

    langs

--- app.py
+++ app.py
@@ -1,11 +1,13 @@
 from flask import Flask, redirect, render_template, request
-from db import db_session as db , User
+from db import db_session as db, User
 app = Flask(__name__)
+
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
     if request.method == 'POST':
-        user = db.query(User).filter(User.username == request.form['username']).first()
+        user = db.query(User).filter(User.username ==
+                                     request.form['username']).first()
         if user is None or user.password != request.form['password']:
             return render_template('index.html', error="Invalid username or password")
         if user.password == request.form['password']:
@@ -13,25 +15,30 @@ def index():
     else:
         return render_template('index.html')
 
+
 @app.route('/register', methods=['POST', 'GET'])
 def login():
     if request.method == 'POST':
-        user = db.query(User).filter(User.username == request.form['username']).first()
+        user = db.query(User).filter(User.username ==
+                                     request.form['username']).first()
         if user is not None:
             return render_template('signup.html', username_error="Username already exists")
         if request.form['password'] != request.form['confirmpassword']:
             return render_template('signup.html', confirmpassword_error="Passwords do not match")
-        user = User(username=request.form['username'], password=request.form['password'])
+        user = User(username=request.form['username'],
+                    password=request.form['password'])
         db.add(user)
         db.commit()
         return redirect('/')
     else:
         return render_template('signup.html', message="Important")
 
+
 @app.route('/reset', methods=['POST', 'GET'])
 def reset():
     if request.method == 'POST':
-        user = db.query(User).filter(User.username == request.form['username']).first()
+        user = db.query(User).filter(User.username ==
+                                     request.form['username']).first()
         if user is None:
             return render_template('forgot.html', username_error="Username does not exist")
         if request.form['password'] != request.form['confirmpassword']:
@@ -41,6 +48,7 @@ def reset():
         return redirect('/')
     else:
         return render_template('forgot.html')
+
 
 if __name__ == '__main__':
     app.run(debug=True, port=5500)
